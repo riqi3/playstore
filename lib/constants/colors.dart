7ee1bdb8969727a4e91556fc9bda1cdeee9ed46b
riqi3 <enrique.pacudan@usjr.edu.ph>
2022-12-18 02:40:14 +0800

    added top chart tab carousel

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,4 +4,7 @@ const Color iconColor = Color (0xFF051f3a);
 const Color iconBg = Color (0xFFc2e8ff);
 const Color botNavBarBg = Color (0xFFf3f4f9);
 const Color grey1 = Color (0xFF47484d);
+const Color grey2 = Color.fromARGB(255, 188, 191, 204);
 const Color highlight = Color (0xFF0e56d4);
+const Color topChart = Color (0xFFd8e8e3);
+const Color topChart2 = Color.fromARGB(255, 87, 127, 114);
